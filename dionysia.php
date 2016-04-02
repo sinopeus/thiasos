a86de11cd2ba2dcc4cd36e188869468db839ac74
Xavier Goas Aguililla <thiasos@ulyssis.org>
2016-04-02 21:57:16 +0200

    Backup 2 apr 2016

--- dionysia.php
+++ dionysia.php
@@ -33,7 +33,7 @@
 
 <div class="dionysia-content">
  <div class="section">
-   <p><h3><a name="dionysia">Dionysia?</a></h3>
+   <p><h2><a name="dionysia">Dionysia?</a></h2>
    </p>
    <p><em>Poogt Poseidon je tocht van de zesde verdieping naar huis al bijna tien jaar lang te saboteren? Of ben je uiteindelijk thuis geraakt en speelt de zoete herinnering aan het Leuvens strijdgewoel je parten? Heb je de overgang naar het zomeruur slecht verwerkt? Tors je een overdreven hoeveelheid zwarte gal mee in je lichaam? Kortom: heb je de blues? </em>
    </p>
@@ -49,12 +49,12 @@
  </div>
 
  <div class="section">
-   <p><h3><a name="programma">Programma</a></h3>
+   <p><h2><a name="programma">Programma</a></h2>
    </p>
    <p><strong><span style="font-variant: small-caps;">Dinsdag 28 april</span></strong>
    </p>
-   <p>Opening</p>
    <blockquote>
+     Opening</br>
      Jonna (14-16 j.) - &quot;Zonder Matthew&quot;</br>
        De studenten Grieks/Latijn van de KU Leuven - &quot;Phaedra&quot;</br>
      Jonna (18-20 j.) - &quot;Bloot en bloed&quot;</br>
@@ -69,13 +69,72 @@
  </div>
  
  <div class="section">
-   <p><h3><a name="deelnemers">Deelnemers</a></h3>
+   <p><h2><a name="deelnemers">Deelnemers</a></h2>
    </p>
    <!-- <p>Toevoegen? Zie voorbeeld in bijlage</p> -->
+   
+   <p><strong><span style="font-variant: small-caps;">Dinsdag 28 april</span></strong></p>
+   <p><strong><h3>1. Zonder Matthew (JONNA)</h3> </strong></p>
+
+
+   <p>Matthew had er niet beter op gevonden om op reis te gaan .</p>
+   <p>Ons geloof kreeg een knauw, waar zijn de de goden gebleven, onze bescherming , bestaan ze wel echt?</p>
+   <p>Die vervloekte mannen, wat denken ze wel dat ze alles voor het zeggen hebben.</p>
+   <p>Weg met ze , vooruit met de geit, we maken onze eigen wereld los van mannen en goden.</p>
+   <p>Weg van de bestaande normen en waarden.</p>
+   <p><strong>Regie:</strong> Pol Pauwels</p>
+   <p><strong>Acteurs:</strong> Coraline Kusters, Marthe Raeymaekers, Tida Weemeeuw, Remco Revalk</p>
+   
+   <p><strong><h3>2. Phaedra (Studenten Grieks/Latijn KU Leuven)</h3> </strong></p>
+   <p><strong>Synopsis</strong>: Niemand moet ooit wagen om Aphrodite te weerstreven; wie het toch doet, zal daar haar straf voor ondergaan. En haar woede keert zich nu tegen Theseus’ zoon, Hippolytus! Maar wat met het werktuig van Aphrodite’s wraak op Hippolytus? Wanneer haar liefde en haar eer tegenover elkaar komen te staan, moet Phaedra alles doen wat ze maar kan om haar geheim te bewaren. Tot ze in een hoek gedreven wordt.</p>
+   <p>Een bewerking van Euripides en Seneca waarin Phaedra’s tweestrijd en onschuld de aandacht krijgen.</p>
+   <p><strong>Tekst en regie:</strong>
+   Simon Van Roost</p>
+   <p><strong>Dramatis personae</strong>:
+   <p><strong>Phaedra</strong> Aurora Van Bel</p>
+   <p><strong>Verzorgster</strong> Astrid De Smet</p>
+   <p><strong>Hippolytus</strong> Pieter Goffin</p>
+   <p><strong>Theseus</strong> Wim Nys</p>
+   <p><strong>Aphrodite</strong> Sten Van Gorp</p>
+   <p><strong>Artemis</strong> Sten Van Gorp</p>
+   <p><strong>Bode</strong> Maxime Maleux</p>
+   <p><strong>Jachtvrienden</strong> Andy Peetermans, Matthias Strauven</p>
+   <p><strong>Burgers</strong> Margot Leblanc,Alexandra Vereeck</p>
+   <p><strong>Slaven</strong> Simon Van Roost, Leontien Vanderschelden</p>
+   <p><strong>Koorzangster</strong> Maria Clara Geurs</p>
+   <p><strong><h3>3. Bloot en Bloed (JONNA)</h3></strong></p>
+   <p>JONNA vzw uit Wilsele biedt tijdens het schooljaar toneellessen aan voor jongeren vanaf 9 jaar.</p>
+   <p>Om de groepen van mekaar te onderscheiden krijgen ze geen nummer maar kiezen ze zelf een ludieke naam. De Kapstokken brengen een bewerking van De Baccanten onder de titel “Bloot en bloed”. Met een beperkte bezetting proberen we het verhaal te spelen van Dionysos die met zijn volgelingen naar Thebe komt om zich daar als god te manifesteren. Maar hij komt vooral om zijn moeder Semele te wreken. Wanneer koning Pentheus hem hardhandig wil aanpakken zal hij deze wraak op geraffineerde manier ten uitvoer brengen. Agave, de moeder van Pentheus, zal in een roes haar zoon voor een roofdier aanzien en doden. Dan komt ze tot inkeer en beseft ze wat ze gedaan heeft.</p>
+   <p><strong>Begeleiding:</strong> Hendrik Van Eycken</p>
+   <p><strong>Spelers:</strong> Valerie Paredis, Marie Peeters, Eva Simon, Bas Van Hoeck, Piter Hillewaert, Milo Podolski, Julie Behaegel, Jelle Debille </p>
+   <p><strong><span style="font-variant: small-caps;">Woensdag 29 april</span></strong>
+   </p>
+   <p><strong><h3>1. Een veldslag om een hart (Academie MWD Gooik)</h3></strong></p>
+   <p>Helena, koningin en mooiste vrouw van Griekenland, heeft haar man Menelaos en dochter Hermione achtergelaten om haar nieuwe liefde Alexandros te volgen naar Ilion, beter bekend als Troje. Menelaos overtuigt alle koningen en helden van Griekenland om ten strijde te trekken en zijn vrouw terug te halen. Wat voorgesteld wordt als een avontuurlijk uitstapje, een kwestie van een paar weken vooraleer ze terug zullen keren met schepen bol van zilver en goud, draait heel anders uit. Agamemnon wordt verplicht zijn dochter te offeren, om wind af te dwingen voor de honderden zeilschepen. En aan Ilion wordt niet minder dan tien jaar lang een bittere strijd gevoerd met grote verliezen aan beide zijden en zonder dat deze beslist wordt. Tot op een dag de Griekse kampen er volledig verlaten bijliggen en er enkel een groot houten paard voor de poort staat ...<br />In zijn reeks van 'Kleine klassiekers' zet Michael de Cock deze keer zijn tanden in de Ilias van Homeros en maakt er een verteerbare tekst van. Hij kiest daarbij voor het standpunt van Helena zelf, wat natuurlijk toelaat haar gevoelens bij dit alles aan bod te laten komen. Het wordt voorgesteld alsof zij haar verhaal vertelt aan haar dochter Hermione, al raakt dat hier en daar toch wel ondergesneeuwd onder de gebeurtenissen</p>
+   <p><strong>Acteurs:</strong> Kaisa Vannimmen, Lore Bonte, Joke Billens ,Kato Vankelecom, Marie Claes, Joran Parent, Sofie Cerstelotte, Fien Breynaert, Remke Van Cauwelaert, Nina Van Praet, Anja Peeters</p>
+   <p><strong>Regie/ bewerking tekst:</strong> Sophie Verbeke</p>
+   <p><strong>Choreografie:</strong> Els Duponchelle</p>
+   <p><strong><h3>2. Tongen? (Klassieke Kring UGent)</h3></strong></p>
+   <p>Twee zusjes en één echtgenoot, meer heeft een goed verhaal vaak niet
+   nodig. In een eigen moderne bewerking brengt de KK dit jaar een verhaal dat
+   ook Ovidius inspireerde voor zijn Metamorphosen. Een klassiek verhaal over
+   hoe de ideale echtgenoot Tereus, eer en macht(smisbruik) de twee zussen
+   Procne en Philomela ver uit elkaar drijft, maar uiteindelijk ook weer
+   samenbrengt. Liefde in haar vele vormen of gewoon your everyday
+   driehoeksverhouding, kortom een verhaal dat nog steeds brandend actueel is
+   aangekruid met een flinke portie muziek en dans.</p>
+   <p><strong>Regie:</strong> Oettie Devriese</p>
+   <p><strong>Choreo:</strong> Silke Bomberna</p>
+   <p><strong>Spel:</strong> Matilde De Cooman, Marie Jacobs, Tijl De Bruyn</p>
+   <p><strong>Tekst:</strong> Lisa Verleyen, Silke Bomberna</p>
+   <p><strong>Zang:</strong> Mieke D'hondt, Elisabeth De Backer</p>
+   <p><strong>Piano:</strong> Laura Versyp</p>
+
+   
  </div>
 
  <div class="section">
-   <p><h3><a name="jury">Jury</a></h3>
+   <p><h2><a name="jury">Jury</a></h2>
    </p>
    <p>Wie wordt de winnaar van de Dionysia 2015?</p>
    <p>De deelnemende stukken worden beoordeeld door een driekoppige jury, bestaande uit een vertegenwoordiger van Thiasos, een vertegenwoordiger van de Klassieke Kring en een buitenstaander uit de theatersector.</p>
@@ -84,7 +143,7 @@
  </div>
  
  <div class="section">
-   <p><h3><a name="tickets">Tickets</a></h3>
+   <p><h2><a name="tickets">Tickets</a></h2>
    </p>
    <p><em><strong>Wat kost een ticket?</strong></em>
    </p>
@@ -105,7 +164,7 @@
  </div>
  <div class="section">
    
-   <p><h3><a name="praktisch">Praktisch</a></h3>
+   <p><h2><a name="praktisch">Praktisch</a></h2>
    </p>
    <p>De Dionysia gaan dit jaar door op dinsdag 28 en woensdag 29 april, en wel in het Heilig-Hartinstituut (Naamsesteenweg 355, Heverlee).</p>
    <p>Beide dagen worden de stadspoorten geopend om 18.30 u. en begint de voorstelling om 19 u.</p>
